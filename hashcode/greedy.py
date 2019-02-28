14365967a6c491b11fb1275541bfeae3159b758a
Tiago Koji Castro Shibata <tiago.shibata@gmail.com>
2019-02-28 16:13:06 -0300

    Greedy solution

---
+++ hashcode/greedy.py
@@ -0,0 +1,31 @@
+from collections import deque
+
+from hashcode.utils import *
+
+
+def delete_nth(d, n):
+    # TODO optimize
+    d.rotate(-n)
+    d.popleft()
+    d.rotate(n)
+
+
+def solve(photos):
+    assert all(x[0] == 'H' for x in photos)
+    photos = deque(x[1] for x in photos)
+    solution = [photos.popleft()]
+
+    while photos:
+        best = best_index = 0
+        # best_index = max(range(len(photos)), key=score_tags(solution[-1], photos.__getitem__)))
+        for i, photo in enumerate(photos):
+            # score = max(photos, key=lambda x: score_tags(solution[-1], x))
+            score = score_tags(solution[-1], photo)
+            if score > best:
+                best = score
+                best_index = i
+        print(best, len(photos), score_tags(solution[-1], photos[best_index]), len(solution[-1] & photos[best_index]), len(solution[-1] - photos[best_index]), len(photos[best_index] - solution[-1]))
+        solution.append(photos[i])
+        delete_nth(photos, i)
+        if len(photos) % 20:
+            print(len(photos))
--- hashcode/utils.py
+++ hashcode/utils.py
@@ -3,6 +3,11 @@ def load(filename):
         l = f.readlines()
     entries = [x.split() for x in l[1:]]
     entries = [(x[0], set(x[2:])) for x in entries]
+    return entries
+
+
+def score_tags(i, j):
+    return min(len(i & j), len(i - j), len(j - i))
 
 
 def score_two_slides(i, j, images):
@@ -10,3 +15,9 @@ def score_two_slides(i, j, images):
     tags1 = list(set(images[i]) - set(images[j]))
     tags2 = list(set(images[j]) - set(images[i]))
     return min(len(intersect), len(tags1), len(tags2))
+
+
+# def format_submission(photos, solution, filename):
+#     with open(filename, 'w') as f:
+#         f.write('')
+#         for x in solution:
